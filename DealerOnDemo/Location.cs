09781f5eeaeb99c8534104436e615fc5e8b23b0f
Liquid Falme <sansari1@cox.net>
2019-09-08 10:43:51 -0400

    First Local commit

---
+++ DealerOnDemo/Location.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Drawing;
+
+namespace DealerOnDemo
+{
+    public class Location
+    {
+        public enum direction { N, S, E, W};
+        private direction _heading;
+
+        public direction Heading
+        {
+            get { return _heading; }
+            set { _heading = value; }
+        }
+
+
+        private int[,] _terrainGrid = new int [5,5];
+        public int[,] TerrainGrid
+        {
+            get { return _terrainGrid; }
+            set { _terrainGrid = value; }
+        }
+        public void Coordinates(int x, int y)
+        {
+            this._terrainGrid[x, y] = 1;
+            this.Heading = 0;
+        }
+        public Location()
+        {
+            this.TerrainGrid[0, 0] = 1;
+            this.Heading = direction.N;
+        }
+        public Location(int x, int y, direction dir)
+        {
+            this._terrainGrid[x, y] = 1;
+            this._heading = dir;
+        }
+    }
+}
---
+++ DealerOnDemo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DealerOnDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Location currentLoc = new Location();
+            printRoverStatus(currentLoc);
+            List<Location> goToRequests = new List<Location>();
+            goToRequests.Add(new Location(2,3,Location.direction.E));
+            printRoverStatus(goToRequests.First());
+        }
+        private static void printRoverStatus(Location loc)
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    if (loc.TerrainGrid[i,j]==1)
+                        Console.WriteLine("I am currently at coordinates: {0} {1} facing:{2} direction",i,j,loc.Heading);
+                }
+            }
+        }
+    }
+}
